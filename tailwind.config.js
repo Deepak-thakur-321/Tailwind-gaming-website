4eda06b0686fd3ff9b5caaec67dd687159d2f5ba
DeepakThakur <deeepakthakur17756@gmail.com>
2025-06-21 17:13:50 +0530

    changes images path and add correct file paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 // tailwind.config.js
 module.exports = {
- content: ["./public/**/*.html", "./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./public/**/*.html", "./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: { extend: {} },
   plugins: [],
 }
+
